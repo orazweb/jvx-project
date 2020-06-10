529a74e365c23ac352ade68fe47aee4082e2a155
xabi <xavier.guezala@live.fr>
2020-06-10 11:47:52 +0200

    initial project

---
+++ raverplayer/src/main/java/com/jvxplayer/raverplayer/RaverplayerApplication.java
@@ -0,0 +1,13 @@
+package com.jvxplayer.raverplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaverplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaverplayerApplication.class, args);
+	}
+
+}
---
+++ raverplayer/src/test/java/com/jvxplayer/raverplayer/RaverplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvxplayer.raverplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaverplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
