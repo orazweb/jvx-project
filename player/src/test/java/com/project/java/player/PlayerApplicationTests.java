5fff40e471c8afc1a80b0375f2c8d711a989f779
Valou <devcodpanda@gmail.com>
2020-06-11 09:47:37 +0200

    add header/footer v1

---
+++ player/src/test/java/com/project/java/player/PlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.java.player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
