82d992e9a7779d7ca6d8ee9679059e85ce3a7484
orazweb <jer.paoli@gmail.com>
2020-06-11 19:23:23 +0200

    demmarage du projet

---
+++ disco_project/src/main/java/com/wildcodeschool/disco_project/DiscoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.disco_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoProjectApplication.class, args);
+	}
+
+}
---
+++ quest-spring-thymeleaf/src/main/java/com/wildcodeschool/questspringthymeleaf/controller/PageController.java
@@ -0,0 +1,4 @@
+package com.wildcodeschool.questspringthymeleaf.controller;
+
+public class PageController {
+}
