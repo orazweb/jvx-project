4e646d37ea9e79c27ce6f759dadcadb6bc82ceca
xabi <xavier.guezala@live.fr>
2020-06-12 15:51:28 +0200

    work

---
+++ disco_project/src/main/java/com/wildcodeschool/disco_project/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.wildcodeschool.disco_project.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+public class AppConfig {
+    public SqlConfig mysql;
+
+
+}
---
+++ disco_project/src/main/java/com/wildcodeschool/disco_project/config/SqlConfig.java
@@ -0,0 +1,8 @@
+package com.wildcodeschool.disco_project.config;
+
+public class SqlConfig {
+
+    public String url;
+    public String username;
+    public String password;
+}
