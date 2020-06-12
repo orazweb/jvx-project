a5f0f7cc2ef7383ea2cdef559a4495ee640ff41f
xabi <xavier.guezala@live.fr>
2020-06-12 08:35:55 +0200

    add somme tracks

--- player/src/main/java/com/project/java/player/Repository/TrackRepo.java
+++ player/src/main/java/com/project/java/player/Repository/TrackRepo.java
@@ -12,7 +12,11 @@ public class TrackRepo implements TrackDao {
     @Override
     public List<Track> findAll(){
         List<Track> trackList = new ArrayList<>();
-        trackList.add(new Track(1, "Beat It", 1, 10, 4, 10, 1, 1990));
+        trackList.add(new Track(1, "Beat It", 1, 10, 1, 10, 1, 1990));
+        trackList.add(new Track(2, "Thunderstruck", 1, 11, 3, 11, 2, 1990));
+        trackList.add(new Track(1, "Amerika", 3, 12, 3, 12, 3, 2004));
+        trackList.add(new Track(1, "Me Gustas Tu", 6, 13, 7, 13, 4, 2001));
+        trackList.add(new Track(1, "Vanessa", 6, 14, 4, 14, 3, 1996));
         return trackList;
     }
 
